99058c160d9f68de9404b2982c20a99c10a8fbdf
supawich_r <supawich_r@elearning.cmu.ac.th>
2021-07-29 22:39:29 +0700

    1.5

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,9 +1,31 @@
 <template>
   <p>Regstration form here</p>
+  <!-- สร้างปุ่ม -->
+  <button @click="register">Register Me!</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'], // <-- Inject the Global Store
+  // register function
+  methods: {
+    register() {
+      //Assuming successful API call to register them
+
+      //Set a flash message to appear on the next page loaded which says
+      // 'You are successfully registered for ' + this.event.title
+
+      //ขึ้้นโชว์ข้อความ
+      this.GStore.flashMessage =
+        'You are successfully registered for ' + this.event.title
+      setTimeout(() => {
+        //After 3 remove it
+        this.GStore.flashMessage = ''
+      }, 3000)
+
+      //Set a flash message to appear on the next page loaded which says
+    }
+  }
 }
 </script>
